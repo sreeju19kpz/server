9fb74d725db42375163500c669f7c44821256ad7
sreeju19kpz <35135394+sreeju19kpz@users.noreply.github.com>
2023-11-22 01:02:23 +0530

    uninstalled firebase

--- app.js
+++ app.js
@@ -3,25 +3,22 @@ const app = express();
 const posts = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const cors = require("cors");
-const functions = require('firebase-functions')
-
 require("dotenv").config();
 
 app.use(cors("http://192.168.0.102:3000"));
 
 app.use(express.static("../client/build"));
 app.use(express.json());
+
 app.get("/hello", (req, res) => {
   res.send("a");
 });
 app.use("/api/v1/posts", posts);
 
-const port = 3001;
-
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log("server on", port);
     });
   } catch (err) {
@@ -29,4 +26,4 @@ const start = async () => {
   }
 };
 
-exports.api = functions.https.onRequest(app);
+start();
